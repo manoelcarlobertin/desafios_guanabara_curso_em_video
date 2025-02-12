d749791a6f7c9eceda5b2d0a106e3635954592e0
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-02-12 11:38:08 -0300

    criei CONSTANTES uma pra dias e outra pra km

--- aluguel_carro.rb
+++ aluguel_carro.rb
@@ -1,9 +1,11 @@
 print "Informe a quantidade de dias alugados: "
 dias = gets.to_i
+PRECO_DIA = 60
 
 print "Informe a quantidade de quilômetros rodados: "
 km = gets.to_f
+PRECO_KM = 0.15
 
-total = (dias * 60) + (km * 0.15)
+total = (dias * PRECO_DIA) + (km * PRECO_KM)
 
 puts "Total a pagar: R$#{'%.2f' % total}"
