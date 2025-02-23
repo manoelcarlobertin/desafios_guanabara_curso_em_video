4cecb0c2a8ae7821de2bcc8a8e0d8f6d77d20aef
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-02-23 20:53:36 -0300

    incrementei as 3 classificações de uma PA.

--- aritmetica.rb
+++ aritmetica.rb
@@ -9,3 +9,10 @@ pa = (0..9).map { |c| n + r * c } # * for * por map → Mais idiomático e funci
 puts pa.join(" -> ") + " -> FIM"
 
 # Adição de " -> FIM" → Indica o final da sequência.
+if r.zero?
+  puts "PA do tipo: CONSTANTE com #{n} termos."
+elsif r.positive?
+  puts "PA do tipo: CRESCENTE com #{n} termos."
+else
+  puts "PA do tipo: DECRESCENTE com #{n} termos."
+end
