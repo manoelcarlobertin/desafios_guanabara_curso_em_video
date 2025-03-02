9645b61eed72f79039d0ed001ec84ae857a311cb
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-03-02 20:07:45 -0300

    esse código está funcionando porém precisa de ajustes para que fique mais modular e fácil de manutenção

--- orcamento_produtos.rb
+++ orcamento_produtos.rb
@@ -5,11 +5,15 @@ require 'tty-prompt'
 require 'json'
 require 'prawn'
 require 'date'
+require 'active_support'
+require 'active_support/core_ext'
 
 class ShoppingCart
-  attr_accessor :products, :prices
-
   FILE_PATH = 'shopping_history.json'.freeze
+  DISCOUNT_THRESHOLD = 100
+  DISCOUNT_RATE = 0.05
+
+  attr_accessor :products, :prices
 
   def initialize
     @products = []
@@ -20,13 +24,12 @@ class ShoppingCart
 
   def start
     show_header('🛒 Bem-vindo à Leroy Marlin')
-
-    load_history if File.exist?(FILE_PATH)
+    load_history
 
     loop do
-      product = load_product
-      price = load_price
-      apply_discount(price, product)
+      product = request_input('🏷️ Nome do Produto:', :yellow) { |input| !input.empty? }
+      price = request_price
+      price = apply_discount(price) { |p| p >= DISCOUNT_THRESHOLD }
 
       @products << product
       @prices << price
@@ -34,17 +37,53 @@ class ShoppingCart
       break unless continue_shopping?
     end
 
-    save_to_json
-    show_summary
-    generate_pdf
+    finalize_purchase
   end
 
   private
 
   def show_header(text)
-    puts '=' * 60
-    puts text.center(60).colorize(:magenta).bold
-    puts '=' * 60
+    puts '=' * 70
+    puts text.center(70).colorize(:magenta).bold
+    puts '=' * 70
+  end
+
+  def request_input(prompt_text, color)
+    loop do
+      print "#{prompt_text} ".colorize(color)
+      input = gets.chomp.strip.capitalize
+      return input if block_given? ? yield(input) : true
+
+      show_message('⚠️ Entrada inválida! Tente novamente.', :red)
+    end
+  end
+
+  def format_currency(value)
+    "R$#{'%.2f' % value}".gsub('.', ',')
+  end
+
+  def request_price
+    request_input('💰 Preço:', :green) do |input|
+      input.match?(/^\d+(\.\d{1,2})?$/) && input.to_f.positive?
+    end.to_f
+  end
+
+  def continue_shopping?
+    @prompt.yes?('➕ Deseja registrar outro produto?')
+  end
+
+  def apply_discount(price)
+    return price unless block_given? && yield(price)
+
+    discounted_price = (price * (1 - DISCOUNT_RATE)).round(2)
+    show_message("🎯 Desconto de 5% aplicado! Novo valor: R$#{'%.2f' % discounted_price}", :green)
+    discounted_price
+  end
+
+  def finalize_purchase
+    save_to_json
+    show_summary
+    generate_pdf
   end
 
   def show_message(text, color = :cyan)
@@ -52,105 +91,84 @@ class ShoppingCart
     sleep(0.5)
   end
 
-  def load_product
-    loop do
-      print '🏷️ Nome do Produto: '.colorize(:yellow)
-      product = gets.chomp.strip.capitalize
-      return product unless product.empty?
-
-      show_message('⚠️ O nome do produto não pode estar vazio!', :red)
-    end
-  end
-
-  def load_price
-    loop do
-      print '💰 Preço: '.colorize(:green)
-      input = gets.chomp
-
-      if input.match?(/^\d+(\.\d{1,2})?$/) && input.to_f.positive?
-        return input.to_f
-      else
-        show_message('❌ Apenas preços positivos são permitidos.', :red)
-      end
-    end
-  end
-
-  def continue_shopping?
-    choices = { 'Sim' => true, 'Não' => false }
-    @prompt.select('➕ Deseja registrar outro produto?', choices)
-  end
-
-  def apply_discount(price, product)
-    if price >= 1000
-      show_message("🎯 Desconto de 10% aplicado para #{product}!", :green)
-      price *= 0.9
-    end
-    price
-  end
-
   def show_summary
     @spinner.auto_spin
     sleep(2)
     @spinner.success('✅ Concluído!')
 
-    show_header("📋 Resumo da Compra - #{Time.now.strftime('%d/%m/%Y %H:%M:%S')}")
+    show_header("📋 Orçamento da Compra - #{Time.now.strftime('%d/%m/%Y %H:%M:%S')} - Vendedor: Carlos")
 
-    puts "🛒 Produtos Comprados (#{@products.size} itens):".colorize(:blue)
-    @products.sort.each_with_index do |product, index|
-      puts "#{index + 1}. #{product}".colorize(:cyan)
-    end
+    puts "🛒 Produtos Orçados (#{@products.size} itens):".colorize(:blue)
+    @products.each_with_index { |product, index| puts "#{index + 1}. #{product} - R$#{'%.2f' % @prices[index]}".colorize(:cyan) }
 
-    puts "💵 Valor total: R$#{'%.2f' % @prices.sum}".colorize(:green)
-    cheapest = @products[@prices.index(@prices.min)]
-    puts "🔥 Produto mais barato: #{cheapest} custando R$#{'%.2f' % @prices.min}".colorize(:yellow)
-    puts "💪 Produtos acima de R$1000: #{@prices.count { |price| price >= 1000 }}".colorize(:light_red)
+    total = @prices.sum
+    cheapest, = @prices.minmax
 
-    puts '=' * 60
-    show_message('🎯 Obrigado pela preferência! Até logo!', :blue)
+    puts "💵 Valor total: R$#{'%.2f' % total}".colorize(:green)
+    puts "🔥 Produto mais barato #{@products[@prices.index(cheapest)]} custando R$#{'%.2f' % cheapest}".colorize(:yellow)
+    puts "💪 Produtos acima de R$100: #{@prices.count { |price| price >= DISCOUNT_THRESHOLD }}".colorize(:light_red)
+    puts '=' * 70
+
+    show_message('🎯 Obrigado pela preferência! * Tudo para Obras * Whatzap 21 998742626', :blue)
   end
 
   def save_to_json
     data = {
-      date: Time.now,
+      date: Time.now.strftime('%d/%m/%Y %H:%M:%S'),
       products: @products,
       prices: @prices
     }
 
-    File.open(FILE_PATH, 'a') do |file|
-      file.puts(JSON.generate(data))
-    end
-    show_message('💾 Compra salva com sucesso!', :green)
+    File.open(FILE_PATH, 'a') { |file| file.puts(JSON.pretty_generate(data)) }
+    show_message('💾 Orçamento de Produtos salvo com sucesso!', :green)
   end
 
   def load_history
-    show_message('📄 Carregando Histórico...', :yellow)
-    file = File.read(FILE_PATH)
-    history = JSON.parse(file)
+    return unless File.exist?(FILE_PATH)
 
-    history.each do |purchase|
+    show_message('📄 Processando Orçamento...', :yellow)
+
+    File.foreach(FILE_PATH) do |line|
+      purchase = JSON.parse(line)
       puts "🗓️ #{purchase['date']}"
-      purchase['products'].each_with_index do |product, index|
-        puts "#{index + 1}. #{product} - R$#{purchase['prices'][index]}"
+      purchase['products'].zip(purchase['prices']).each_with_index do |(product, price), index|
+        puts "#{index + 1}. #{product} - R$#{'%.2f' % price}"
       end
-      puts '-' * 40
+      puts '-' * 70
     end
+  rescue JSON::ParserError
+    show_message('❌ Erro ao carregar orçamento. Arquivo corrompido.', :red)
   end
 
   def generate_pdf
-    Prawn::Document.generate("resumo_compra_#{Time.now.strftime('%Y%m%d')}.pdf") do
-      text 'Resumo da Compra', size: 20, style: :bold
-      move_down 20
+    return show_message('❌ Nenhum produto registrado para gerar PDF!', :red) if @products.empty?
 
-      @products.each_with_index do |product, index|
-        text "#{index + 1}. #{product} - R$#{'%.2f' % @prices[index]}"
+    @products ||= [] # Se a variável for nil, ele cria um array vazio
+    @prices ||= [] # Se já estiver preenchida, ele apenas usa o valor existente
+
+    file_name = "orcamento_compra_#{Time.now.strftime('%Y%m%d')}.pdf"
+    Prawn::Fonts::AFM.hide_m17n_warning = true
+    Prawn::Document.generate(file_name) do
+      text 'Orçamento da Compra', size: 20, style: :bold
+      move_down 20
+      text 'Produtos Registrados:', size: 15, style: :bold
+      text '-' * 100
+
+      if @products&.any?
+        @products.each_with_index do |product, index|
+          text "#{index + 1}. #{product} - R$#{'%.2f' % @prices[index]}"
+        end
+      else
+        text 'Nenhum produto registrado.'
       end
 
       move_down 20
-      text "Valor Total: R$#{'%.2f' % @prices.sum}", size: 15, color: '009900'
-      text "Data da Compra: #{Time.now.strftime('%d/%m/%Y %H:%M:%S')}"
+      text "Valor Total: #{format_currency(@prices.sum)}", size: 15, color: :green
+
+      text "Data do Orçamento: #{Time.now.strftime('%d/%m/%Y %H:%M:%S')}"
     end
 
-    show_message('📄 PDF gerado com sucesso!', :green)
+    show_message("📄 PDF '#{file_name}' gerado com sucesso!", :green)
   end
 end
 
